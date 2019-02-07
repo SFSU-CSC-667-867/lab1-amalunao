cf0e287ce0c2e06bde1c3425431b16ca5e46cd50
amalunao <33701477+amalunao@users.noreply.github.com>
2019-02-06 20:37:52 -0800

    Add files via upload

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,32 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
+ const arrayTest =[9, 3, 4];
+ arrayTest[5] = 9;
+ console.log(arrayTest);
+ arrayTest.forEach(n) => {
+     console.log(n);
+ }};
 
 // use map
-
+const newArray = arrayTest.map( => i + 1);
+console.log(newArray)
 // use pop
+newArray.pop();
+console.log(newArray);
 
 // use push
+newArray.push(8);
+console.log(newArray);
 
 // use shift
+newArray.shift();
+console.log(newArray);
 
 // use unshift
+newArray.unshift();
+console.log(newArray);
 
 // use filter
+newArray.filter();
+console.log(newArray);
